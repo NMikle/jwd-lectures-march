4c9e11c13c9ff46d93ea7f145ded670b58234b3d
nmikle <nekrasovmikle@gmail.com>
2021-03-18 21:48:45 +0300

    log4j2 configuration

--- src/com/epam/jwd/lectures/app/Main.java
+++ src/com/epam/jwd/lectures/app/Main.java
@@ -1,6 +1,17 @@
 package com.epam.jwd.lectures.app;
 
+import com.epam.jwd.lectures.model.Cat;
+import com.epam.jwd.lectures.service.AnimalService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
+        LOGGER.info("Program Started");
+        AnimalService.saySomething(new Cat("Barsik"));
+        LOGGER.info("Program Ended");
     }
 }
