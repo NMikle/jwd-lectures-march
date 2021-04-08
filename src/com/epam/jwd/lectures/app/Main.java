8388284e4dce97b901202fc73015782cf3443963
nmikle <nekrasovmikle@gmail.com>
2021-04-08 19:28:56 +0300

    collections cool stuff

--- src/com/epam/jwd/lectures/app/Main.java
+++ src/com/epam/jwd/lectures/app/Main.java
@@ -1,24 +1,26 @@
 package com.epam.jwd.lectures.app;
 
-import com.epam.jwd.lectures.exception.PreProcessorException;
-import com.epam.jwd.lectures.model.Doctor;
-import com.epam.jwd.lectures.model.DoctorType;
-import com.epam.jwd.lectures.model.DoctorsUniversity;
-import com.epam.jwd.lectures.model.License;
+import com.epam.jwd.lectures.model.Color;
+import com.epam.jwd.lectures.model.Human;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
-        final DoctorsUniversity university = DoctorsUniversity.newInstance();
-        final License license = university.newLicense("nurse");
-        final Doctor doctor;
-        try {
-            doctor = university
-                    .newDoctor(DoctorType.NURSE, "Alice", license);
-            doctor.healPatient(null);
-        } catch (PreProcessorException e) {
-            System.out.println("Could not create doctor");
-        }
+//        final List<String> strings = Arrays.asList("Hey", "you");
+//        strings.add("out");
+//        strings.forEach(System.out::println);
+//
+//        final List<Color> colors = Collections.singletonList(Color.RED);
+//        final List<Human> emptyList = Collections.emptyList();
+
+//        final Set<Color> colors = EnumSet.range(Color.GREEN, Color.WHITE);
+//        colors.forEach(System.out::println);
     }
 
 }
