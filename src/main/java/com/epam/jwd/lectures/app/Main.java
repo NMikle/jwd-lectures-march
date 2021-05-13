cefc517eb0c28c937abf2041683124e92a81120d
nmikle <nekrasovmikle@gmail.com>
2021-05-13 20:18:21 +0300

    jdbc introduction

--- src/main/java/com/epam/jwd/lectures/app/Main.java
+++ src/main/java/com/epam/jwd/lectures/app/Main.java
@@ -1,106 +1,59 @@
 package com.epam.jwd.lectures.app;
 
-import com.epam.jwd.lectures.concurrency.StaleCount;
+import com.epam.jwd.lectures.model.AppUser;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Enumeration;
 
 public class Main {
 
-    static int x = 0;
-    static int y = 0;
-    static int a = 0;
-    static int b = 0;
-
-    public static void main(String[] args) throws ExecutionException {
-        System.out.println("program start");
-
-        //first thread creation variant
-//        final SimpleRunnable firstRunnable = new SimpleRunnable();
-//        final SimpleRunnable secondRunnable = new SimpleRunnable();
-//
-//        new Thread(firstRunnable).start();
-//        new Thread(secondRunnable).start();
-
-        //second thread creation variant
-//        final CalculationThread t1 = new CalculationThread(3);
-//        final CalculationThread t2 = new CalculationThread(8);
-//
-//        t1.start();
-//        t2.start();
-//        t1.join();
-//        t2.join();
-//        System.out.printf("First result: %d, second result: %d\n", t1.result, t2.result);
-//        t1.isAlive() == false //another way to detect thread end
-
-//        race condition
-//        final RaceConditionCounter c1 = new RaceConditionCounter();
-//        IntStream.range(0, 1_000_000).forEach(i -> c1.increment());
-//
-//        final RaceConditionCounter c2 = new RaceConditionCounter();
-//        IntStream.range(0, 1_000_000).parallel().forEach(i -> c2.increment());
-//
-//        System.out.printf("First result: %d, second result: %d\n", c1.count, c2.count);
-
-//        stale values
-//        final StaleCount staleCount = new StaleCount();
-//        final Thread t = new Thread(staleCount);
-//        t.start();
-//        Thread.sleep(1_000);
-//        staleCount.setShouldStop(true);
-
-//        reordering
-//        final Thread t1 = new Thread(() -> {
-//            a = 1;
-//            x = b;
-//        });
-//        final Thread t2 = new Thread(() -> {
-//            b = 1;
-//            y = a;
-//        });
-//        t1.start();
-//        t2.start();
-//        t1.join();
-//        t2.join();
-//
-//        System.out.printf("x: %d, y: %d\n", x, y);
-
-
-        final ExecutorService executorService = Executors.newFixedThreadPool(2);
-
-        final Future<Integer> pendingResult = executorService.submit(() -> 2 + 2);
-
-        System.out.println("result computing");
-        if (!pendingResult.isDone()) {
-            System.out.println("still computing");
-            pendingResult.cancel(true);
-        }
-        try {
-            final Integer operationResult = !pendingResult.isCancelled() ? pendingResult.get() : 0;
-
-            System.out.println("done: " + operationResult);
-
-            executorService.shutdown();
-            if (!executorService.awaitTermination(2, TimeUnit.SECONDS)) {
-                executorService.shutdownNow();
+    public static void main(String[] args) {
+        System.out.println("start");
+        registerDrivers();
+        try (final Connection conn = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/testDb", "root", "root");
+             final Statement statement = conn.createStatement();
+             final ResultSet resultSet = statement.executeQuery("select * from app_user")) {
+            while (resultSet.next()) {
+                final AppUser user = new AppUser(resultSet.getLong("id"),
+                        resultSet.getString("u_name"),
+                        resultSet.getInt("u_age"));
+                System.out.printf("found user: %s\n", user);
             }
-        } catch (InterruptedException e) {
+        } catch (SQLException e) {
+            System.out.println("user name read unsuccessfully");
             e.printStackTrace();
-            Thread.currentThread().interrupt();
-            return;
         }
+        deregisterDrivers();
+        System.out.println("end");
+    }
 
-        System.out.println("program end");
-//        Thread.currentThread().getState()
+    private static void registerDrivers() {
+        System.out.println("sql drivers registration start...");
+        try {
+            DriverManager.registerDriver(DriverManager.getDriver("jdbc:mysql://localhost:3306/testDb"));
+            System.out.println("registration successful");
+        } catch (SQLException e) {
+            System.out.println("registration unsuccessful");
+            e.printStackTrace();
+        }
+    }
 
-
-//        final List<Long> someData = new ArrayList<>();
-//        final List<Long> synchronizedData = Collections.synchronizedList(someData); //uses locks (very bad) -- do not use!!!!!
+    private static void deregisterDrivers() {
+        System.out.println("sql drivers unregistering start...");
+        final Enumeration<Driver> drivers = DriverManager.getDrivers();
+        while (drivers.hasMoreElements()) {
+            try {
+                DriverManager.deregisterDriver(drivers.nextElement());
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("unregistering drivers failed");
+            }
+        }
     }
 }
---
+++ src/main/java/com/epam/jwd/lectures/model/AppUser.java
@@ -0,0 +1,53 @@
+package com.epam.jwd.lectures.model;
+
+import java.util.Objects;
+
+public class AppUser {
+
+    private final Long id;
+    private final String name;
+    private final int age;
+
+    public AppUser(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AppUser appUser = (AppUser) o;
+        return age == appUser.age
+                && Objects.equals(id, appUser.id)
+                && Objects.equals(name, appUser.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "AppUser{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+}
